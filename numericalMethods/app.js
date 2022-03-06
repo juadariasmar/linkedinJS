0382ae25c75e24dd3fe56f96dc7825bf802ca5db
juadariasmar <juadarismar@gmail.com>
2022-03-06 14:32:46 -0500

    numeric properties of instances

--- numericalMethods/app.js
+++ numericalMethods/app.js
@@ -11,3 +11,17 @@ console.log("parseFloat: ", parseFloat(num));
 console.log("isNaN: ", isNaN(num));
 
 console.log("isInteger: ", Number.isInteger(num));
+
+
+//*************
+//** Numeric properties of instances
+
+let num2 = 2.5;
+
+console.log("toExponential: ", num2.toExponential(4));
+
+console.log("toFixed: ", num2.toFixed(2));
+
+console.log("toPrecision: ", num2.toPrecision(6));
+
+console.log("toString: ", typeof num2.toString());
