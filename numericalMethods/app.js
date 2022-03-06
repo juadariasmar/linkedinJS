d45f82d1d1c95fba1451ba7506b4b9c6d9470391
juadariasmar <juadarismar@gmail.com>
2022-03-06 14:25:03 -0500

    practice Js

---
+++ numericalMethods/app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+let num = "10.30"
+
+console.log("Number: ", typeof num, typeof Number(num));
+
+console.log("parseInt: ", parseInt(num));
+
+console.log("parseFloat: ", parseFloat(num));
+
+console.log("isNaN: ", isNaN(num));
+
+console.log("isInteger: ", Number.isInteger(num));
