39be5a7ca379c3b2fd4384acefc24d48abc8e53a
juadariasmar <juadarismar@gmail.com>
2022-03-06 14:47:47 -0500

    practice with text strings

---
+++ creatingTextString/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//*************************
+//*** Creating Text String
+
+let country = 'Mexico';
+
+let food = new String('Ceviche');
+
+console.log("The text string have: " + country.length + " letters");
+
+console.log("The text string have: " + food.length + " letters");
