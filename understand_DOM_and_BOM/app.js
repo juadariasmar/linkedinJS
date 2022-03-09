a6b8f71034d2aa9bdaeb40f089d0ce29cf8e8df6
juadariasmar <juadarismar@gmail.com>
2022-03-09 02:13:17 -0500

    js practice

--- understand_DOM_and_BOM/app.js
+++ understand_DOM_and_BOM/app.js
@@ -1,1 +1,28 @@
 'use strict'
+
+//*******************************
+//*** DOM
+
+/*
+let button = document.getElementById("button");
+
+button.addEventListener('click', function() {
+    console.log(window.location.href);
+})
+*/
+
+//*************************************
+//*** Dom properties and methods
+
+let button = document.getElementById("button");
+
+let allP = document.getElementsByTagName("p");
+let pForClass = document.getElementsByClassName("principal")[0].textContent;
+let photo;
+
+button.addEventListener('click', function() {
+    console.log(allP);
+    console.log(pForClass);
+
+
+})
